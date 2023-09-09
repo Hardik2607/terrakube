5d2d93b4f45c5b6e0451782e26fab0b49187cf41
Hardik2607 <62460135+Hardik2607@users.noreply.github.com>
2023-09-10 03:21:13 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,6 +8,6 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  AWS_ACCESS_KEY_ID = var.AWS_ACCESS_KEY_ID
-  AWS_SECRET_ACCESS_KEY = vat.AWS_SECRET_ACCESS_KEY
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = vat.AWS_SECRET_ACCESS_KEY
 }
